d5ff42919f7f986dfb25531d5d856e68cc119298
berfinozkok <berfnozkok@hotmail.com>
2021-08-09 10:41:03 +0300

    latest version

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, cur
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import Form, StringField, PasswordField, SubmitField, validators, TextField
-from wtforms.validators import EqualTo, InputRequired, Length, ValidationError,  DataRequired, Email
+from wtforms.validators import EqualTo, InputRequired, Length, ValidationError,  DataRequired, Email, email_validator, Required
 from flask_bcrypt import Bcrypt
-from email.utils import make_msgidfrom 
 from flask_mail import Mail, Message
 from io import BytesIO
 import onetimepass, pyqrcode, os, base64
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app=Flask(__name__)
 
@@ -49,15 +49,31 @@ class User(db.Model, UserMixin):
         if self.otp_secret is None:
             # generate a random secret
             self.otp_secret = base64.b32encode(os.urandom(10)).decode('utf-8')
-    
-    def __repr__(self):
-        return f'{self.username}:{self.email}:{self.date_created.strftime("%d%m%Y, %H:%M:%S")}'
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_totp_uri(self):
+        return 'otpauth://totp/2FA-Demo:{0}?secret={1}&issuer=2FA-Demo' \
+            .format(self.username, self.otp_secret)
+
+    def verify_totp(self, token):
+        return onetimepass.valid_totp(token, self.otp_secret)
+
     
 class RegistrationForm(FlaskForm):
     username=StringField(label= 'username', validators=[DataRequired(),Length(min=3, max=20)])
     email=StringField(validators=[DataRequired(),Email()])
     password=PasswordField(label= 'Password', validators=[DataRequired(),Length(min=3, max=20)])
-    confirm_password= PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo])
+    confirm_password= PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo(password)])
     submit=SubmitField(label= 'Sign Up', validators=[DataRequired()])
 
     def validate_username(self, username):
@@ -70,6 +86,7 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email=StringField(label='Email', validators=[DataRequired(),Length(min=4, max=20)])
     password=PasswordField(validators=[DataRequired(),Length(min=4, max=20)])
+    token = StringField('Token', validators=[Required(), Length(6, 6)])
     submit=SubmitField(label='Login', validators=[DataRequired()])
 
 class ResetRequestForm(FlaskForm):
@@ -78,7 +95,7 @@ class ResetRequestForm(FlaskForm):
 
 class ResetPasswordForm(FlaskForm):
     password=PasswordField(validators=[DataRequired(),Length(min=4, max=20)])
-    confirm_password= PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo])
+    confirm_password= PasswordField(label='Confirm Password', validators=[DataRequired(), EqualTo(password)])
     submit=SubmitField(label='Change Password', validators=[DataRequired()])
 
 @app.route('/')
@@ -96,65 +113,6 @@ def register():
         flash(f'Account created successfully for {{form.username.data}}')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form=LoginForm()
-    if form.validate_on_submit():
-        user= User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password,form.password.data):
-                login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html',title='Login', form=form)
-
-@app.route('/logout', methods=['GET','POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/dashboard', methods=['GET', 'POST'])
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
-
-def send_mail(user):
-    token=user.get_token()
-    msg=Message('Password Reset Request', recipients=[user.email], sender='noreply@codejana.com')
-    msg.body=f''' To reset your password. Please follow the link below
-
-    {url_for('reset_token', token=token,_external=True)}
-    If you didn't send a password reset request. Please ignore this message.
-
-    
-    '''
-    mail.send(msg)
-
-@app.route('/reset_password', methods=['POST', 'GET'])
-def reset_request():
-    form=ResetRequestForm()
-    if form.validate_on_submit():
-        user=User.query.filter_by(email=form.email.data).first()
-        if user:
-            send_mail(user)
-            flash('Reset request sent. Check your mail', 'success')
-            return redirect(url_for('login'))
-    return render_template('reset_request.html', title='Reset Request', legend='Reset Password')
-
-@app.route('/reset_password/<token>', methods=['POST', 'GET'])
-def reset_token(token):
-    user=User.verify_token(token)
-    if user is None:
-        flash('That is invalid token or expired. Please try again.', 'warning')
-        return redirect(url_for('reset_request'))
-    form=ResetPasswordForm()
-    if form.validate_on_submit():
-        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user.password=hashed_password
-        db.session.commit()
-        flash('Password changed. Please login.', 'success')
-        return(redirect(url_for('login')))
-    return render_template('change_password.html', title="Change Password", legend="Change Password", form=form)
 
 @app.route('/twofactor')
 def two_factor_setup():
@@ -190,6 +148,69 @@ def qrcode():
         'Cache-Control': 'no-cache, no-store, must-revalidate',
         'Pragma': 'no-cache',
         'Expires': '0'}
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user= User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html',title='Login', form=form)
+
+@app.route('/logout', methods=['GET','POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+def send_mail(user):
+    token=user.get_token()
+    msg=Message('Password Reset Request', recipients=[user.email], sender='noreply@codejana.com')
+    msg.body=f''' To reset your password. Please follow the link below
+
+    {url_for('reset_token', token=token,_external=True)}
+    If you didn't send a password reset request. Please ignore this message.
+
+    
+    '''
+    mail.send(msg)
+
+@app.route('/reset_request', methods=['POST', 'GET'])
+def reset_request():
+    form=ResetRequestForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_mail(user)
+            flash('Reset request sent. Check your mail', 'success')
+            return redirect(url_for('login'))
+    return render_template('reset_request.html', title="Reset Request", legend="Reset Password", form=form)
+
+
+@app.route('/reset_request/<token>', methods=['POST', 'GET'])
+def reset_token(token):
+    user=User.verify_token(token)
+    if user is None:
+        flash('That is invalid token or expired. Please try again.', 'warning')
+        return redirect(url_for('reset_request'))
+    form=ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password=hashed_password
+        db.session.commit()
+        flash('Password changed. Please login.', 'success')
+        return(redirect(url_for('login')))
+    return render_template('change_password.html', title="Change Password", legend="Change Password", form=form)
+
+
 
 
 if __name__=='__main__':
